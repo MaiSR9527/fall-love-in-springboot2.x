d5e7c0139e9958392041c6c1f64f81f696cd8746
srMai <maisrcn@qq.com>
2021-07-28 14:52:23 +0800

    jdbctemplate模块

---
+++ boot-datasource-jdbctemplate/src/test/java/com/msr/better/jdbc/TestApplication.java
@@ -0,0 +1,13 @@
+package com.msr.better.jdbc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author MaiShuRen
+ * @site https://www.maishuren.top
+ * @since 2021-04-30 00:00
+ **/
+@SpringBootTest
+public class TestApplication {
+
+}
