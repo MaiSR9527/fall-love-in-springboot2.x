b9e62f53ad0da367bfb31e010bd8c740e38d38b2
shuren mai <maisrcn@qq.com>
2021-08-03 22:26:06 +0800

    do something

--- boot-basic-config/src/main/java/com/msr/better/config/WebConfig.java
+++ boot-basic-config/src/main/java/com/msr/better/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.msr.better.config;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -16,17 +18,27 @@ public class WebConfig {
 
     @Bean
     public TomcatServletWebServerFactory webServerFactory() {
-        TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory(){
+        TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
                 SecurityConstraint securityConstraint = new SecurityConstraint();
                 securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                securityConstraint.set
-                super.postProcessContext(context);
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
             }
         };
-
-
+        webServerFactory.addAdditionalTomcatConnectors(myConnectors());
         return webServerFactory;
+    }
+
+    private Connector myConnectors() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8000);
+        return connector;
     }
 }
--- boot-basic-config/src/main/java/com/msr/better/controller/HelloController.java
+++ boot-basic-config/src/main/java/com/msr/better/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.msr.better.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class HelloController {
             list.add("hello:" + i);
         }
         return list;
+    }
+
+    @GetMapping("/port")
+    public int getPort(@Value("${server.port}")Integer port) {
+        return port;
     }
 }
