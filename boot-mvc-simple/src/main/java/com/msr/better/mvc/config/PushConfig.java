a83493ae74a40ce6fe7fb115ffa872fe7beae88d
maishuren <maisrcn@qq.com>
2022-04-27 21:02:53 +0800

    wait to complete

---
+++ boot-mvc-simple/src/main/java/com/msr/better/mvc/config/PushConfig.java
@@ -0,0 +1,14 @@
+package com.msr.better.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author MaiShuRen
+ * @site https://www.maishuren.top
+ * @since 2022-04-27
+ **/
+@Configuration
+public class PushConfig {
+
+}
