1969ab9f7bab82934a68e351a4ae19c966727c11
maishuren <maisrcn@qq.com>
2024-01-16 10:27:45 +0800

    upgrade jdk version，remove unused module

--- boot-starter/msr-shenyu-spring-boot-starter-client-springmvc/src/main/java/com/msr/better/client/springmvc/MsrShenYuSpringMvcClientConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.msr.better.client.springmvc;
-
-public class MsrShenYuSpringMvcClientConfiguration {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
